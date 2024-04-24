b80f8c61939a8251286b6863e4dc4e5060622d85
MJZF00155619 <00155619@uca.edu.sv>
2024-04-24 12:46:55 -0600

    feat: add create user endpoint

---
+++ src/controllers/userController.js
@@ -0,0 +1,19 @@
+const pool = require('../dbconnection')
+
+const createUser = async(req,res)=>{
+
+    const {name, lastname, email, user_password} = req.body;
+
+    const response = await pool.query('insert into users (names, lastnames, email, user_password) values ($1, $2, $3, $4)', [name, lastname, email, user_password]);
+    console.log(response);
+    res.status(200).json({
+        message: 'UserCreated',
+        body: {
+            users: { name, lastname, email, user_password }
+        }
+    });
+}
+
+module.exports = {
+    createUser,
+}
--- src/index.js
+++ src/index.js
@@ -5,8 +5,7 @@ const port = 3001;
 app.use(express.json());
 app.use(express.urlencoded({extended : false}));
 
-//app.use(require('./routes/'));
-//app.use(require('./routes/'));
+app.use(require('./routes/userRouter'));
 
 app.listen(port);
 console.log('Server on port ', port);
